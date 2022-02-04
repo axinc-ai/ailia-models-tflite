ebbb0bab0d124f4a90fce849f8a25ad8eb7965fd
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-02-04 23:11:01 +0900

    Added benchmark script for windows

---
+++ scripts/download_all_models.bat
@@ -0,0 +1,13 @@
+set OPTION=-b
+cd ..\\
+cd face_detection\\blazeface& python blazeface.py %OPTION%
+cd ..\\..\\face_recognition\\facemesh& python facemesh.py %OPTION%
+cd ..\\..\\hand_recognition\\blazehand& python blazehand.py %OPTION%
+cd ..\\..\\image_classification\\efficientnet_lite& python efficientnet_lite.py %OPTION%
+cd ..\\..\\image_classification\\mobilenetv1& python mobilenetv1.py %OPTION%
+cd ..\\..\\image_classification\\mobilenetv2& python mobilenetv2.py %OPTION%
+cd ..\\..\\image_classification\\resnet50& python resnet50.py %OPTION%
+cd ..\\..\\image_classification\\resnet50& python resnet50.py %OPTION% --float
+cd ..\\..\\image_segmentation\\deeplabv3plus& python deeplabv3plus.py %OPTION%
+cd ..\\..\\object_detection\\yolov3-tiny& python yolov3-tiny.py %OPTION%
+cd ..\\..\\object_detection\\yolox& python yolox.py %OPTION%
