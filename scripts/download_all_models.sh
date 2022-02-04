062ef20a5cd2853e3178e738ac1f1c9b8f1bf735
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-02-04 23:10:06 +0900

    Fix path

--- scripts/download_all_models.sh
+++ scripts/download_all_models.sh
@@ -3,7 +3,7 @@ cd ../
 cd face_detection/blazeface; python3 blazeface.py ${OPTION}
 cd ../../face_recognition/facemesh; python3 facemesh.py ${OPTION}
 cd ../../hand_recognition/blazehand; python3 blazehand.py ${OPTION}
-cd ../../image_classification/efficientnet_lite python3 efficientnet_lite.py ${OPTION}
+cd ../../image_classification/efficientnet_lite; python3 efficientnet_lite.py ${OPTION}
 cd ../../image_classification/mobilenetv1; python3 mobilenetv1.py ${OPTION}
 cd ../../image_classification/mobilenetv2; python3 mobilenetv2.py ${OPTION}
 cd ../../image_classification/resnet50; python3 resnet50.py ${OPTION}
