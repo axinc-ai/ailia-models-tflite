1035b5181cdb0d8fcfeeb44d15f05c2cc1e2fbb2
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-05-17 15:57:43 +0900

    Fix for normal image

--- object_detection/yolox/yolox_utils.py
+++ object_detection/yolox/yolox_utils.py
@@ -142,6 +142,6 @@ def filter_predictions(predictions, raw_img, ratio, nms_thr, score_thr):
     boxes_xyxy /= ratio
     dets = multiclass_nms(boxes_xyxy, scores, nms_thr, score_thr)
 
-    if dets==None:
-        return None
-    return dets.astype(np.float32)
+    if dets is not None:
+        return dets.astype(np.float32)
+    return None
